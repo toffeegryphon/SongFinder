8f72f6922c683f2ce73cf9270f83c2881ab79f2e
Fu Nairen <nairenfu@gmail.com>
2019-04-18 11:55:45 +0800

    Search, Recommend, and Feedback now works. Tidied up created files into a separate folder. TODO left voting.

--- v2/app.py
+++ v2/app.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template, request, redirect, url_for
 
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, HiddenField
 from wtforms.validators import DataRequired
 
-import db
+from werkzeug.contrib.cache import SimpleCache
+
+import db, songs
 
 def create_app():
 	app = Flask(__name__)
@@ -24,10 +26,19 @@ def create_app():
 
 	db.initApp(app)
 
+	cache = SimpleCache()
+
 	class SearchForm(FlaskForm):
 		artist_name = StringField(default = 'george ezra', validators = [DataRequired()])
 
+	class RecommendForm(FlaskForm):
+		form_name = HiddenField('form_name')
+		main_artist = HiddenField('main_artist')
+		artist_name = StringField(default = 'sigrid', validators = [DataRequired()])
+		submit = SubmitField()
+
 	class FeedbackForm(FlaskForm):
+		form_name = HiddenField('form_name')
 		user = StringField(default = 'anonymous')
 		comment = TextAreaField(validators = [DataRequired()])
 		submit = SubmitField()
@@ -35,22 +46,51 @@ def create_app():
 
 	@app.route('/')
 	def base():
-		feedback_form = FeedbackForm()
+		feedback_form = FeedbackForm(form_name = 'feedback')
 		search_form = SearchForm()
+
+		charts = songs.getCharts()[0:10]
+		cache.set('charts', charts)
 
 		if request.args.get('artist_name') is not None:
 			search_form = SearchForm(request.args)
+			
 			artist_name = search_form.artist_name.data
+			artist = songs.buildArtist(artist_name) ##TODO add album
+
+			recommend_form = RecommendForm(
+				form_name = 'recommend', 
+				main_artist = artist_name)
+
+			# if request.args.get('track'):
+			# 	##TODO return in correct sorted order based on prior to refreshing
+			# 	artist = songs.upvote(artistName, request.args.get('track'))
+
+			cache.set('main_artist', artist)
+			recordings = artist.get('recordings')
+			relationships = []
+			for relationship in artist.get('relationships'):
+				print("Relationship: ", relationship)
+				##TODO Fix Somehow this is None
+				print(artist.get('relationships').get(relationship))
+				relationships.append([songs.getArtistName(relationship), relationship, artist.get('relationships').get(relationship)])
+			print(relationships)
+
 			return render_template(
 				'artist.html', 
 				artist_name = artist_name, 
 				feedback_form = feedback_form, 
-				search_form = search_form,)
+				search_form = search_form, 
+				recommend_form = recommend_form, 
+				recordings = recordings, 
+				relationships = relationships
+				)
 
 		return render_template(
 			'base.html', 
 			feedback_form = feedback_form, 
-			search_form = search_form
+			search_form = search_form, 
+			charts = charts
 			)
 
 	@app.route('/', methods = ['POST', 'GET'])
@@ -61,16 +101,53 @@ def create_app():
 		print(request.form)
 
 		if request.method == 'POST':
-			feedback_form = FeedbackForm(request.form)
 
-			user = feedback_form.user.data
-			comment = feedback_form.comment.data
+			if request.form.get('form_name') == 'feedback':
+				feedback_form = FeedbackForm(request.form)
 
-			database.execute('INSERT INTO post (user, comment) VALUES (?, ?)', (user, comment))
-			database.commit()
-			print('success')
+				user = feedback_form.user.data
+				comment = feedback_form.comment.data
 
-			return redirect(url_for('base'))
+				database.execute('INSERT INTO post (user, comment) VALUES (?, ?)', (user, comment))
+				database.commit()
+				print('success')
+
+				return redirect(url_for('base'))
+
+			if request.form.get('form_name') == 'recommend':
+				recommend_form = RecommendForm(request.form)
+				feedback_form = FeedbackForm()
+				search_form = SearchForm()
+
+				main_artist = cache.get('main_artist')
+
+				main_name = recommend_form.main_artist.data
+				second_name = recommend_form.artist_name.data
+
+				print("ADDING RELATIONSHIP")
+				##TODO Optimise
+				main_artist['relationships'][songs.getArtistId(second_name)] = songs.addRelationship(second_name, main_artist.get('artistName'))
+				print(main_artist.get('relationships'))
+
+				relationships = []
+				for relationship in main_artist.get('relationships'):
+					print("Relationship: ", relationship)
+					##TODO Fix Somehow this is None
+					print(main_artist.get('relationships').get(relationship))
+					relationships.append([songs.getArtistName(relationship), relationship, main_artist.get('relationships').get(relationship)])
+
+				print(relationships)
+				cache.set('main_artist', main_artist)
+
+				return render_template(
+					'artist.html', 
+					artist_name = main_name, 
+					feedback_form = feedback_form, 
+					search_form = search_form, 
+					recommend_form = recommend_form, 
+					recordings = main_artist.get('recordings'), 
+					relationships = relationships
+					)
 
 		else:
 			print(request)
---
+++ v2/songs.py
@@ -0,0 +1,364 @@
+import os, urllib.request, json, csv
+from unidecode import unidecode
+from operator import itemgetter
+from enum import Enum
+
+BASE_URL = "https://musicbrainz.org/ws/2/"
+QUERY_ARTIST = "lana+del+rey"
+
+def getArtistName(artistId):
+    print("Requesting name of %s" % (artistId))
+
+    REQUEST_ARTIST_NAME = "%sartist/%s?fmt=json" % (BASE_URL, artistId)
+    print(REQUEST_ARTIST_NAME)
+    requestArtistName = urllib.request.urlopen(urllib.request.Request(REQUEST_ARTIST_NAME)).read().decode("utf-8")
+    artistName = json.loads(requestArtistName).get('name')
+
+    return artistName
+
+def getArtistId(artist, numberOfResults = 1):
+    ##TODO Check if artist name is same as the one obtained from ID
+    print("Requesting %s's artist ID" % (artist))
+
+    QUERY_ARTIST = artist.lower().replace(" ", "+")
+    REQUEST_ARTIST_ID = "%sartist/?query=%s&limit=%s&fmt=json" % (BASE_URL, QUERY_ARTIST, str(numberOfResults))
+    print(REQUEST_ARTIST_ID)
+    requestArtistId = urllib.request.urlopen(urllib.request.Request(REQUEST_ARTIST_ID)).read().decode("utf-8")
+
+    if numberOfResults != 1:
+        artistIds = {}
+        artists = json.loads(requestArtistId).get('artists')
+        for artist in artists:
+            artistIds.update({artist.get('id') : artist.get('name')})
+        return artistIds
+    artistId = json.loads(requestArtistId).get('artists')[0].get('id')
+
+    return artistId
+
+class ArtistInput(Enum):
+    ARTIST_ID = 1
+    ARTIST_NAME = 2
+
+def getSongsByArtistId(artistId, offset=None, songlist=None):
+    ##TODO Fix heroku throws 500 Internal Server Error @ ~ page 12
+    print("Requesting raw list of %s's recordings" % (artistId))
+    if offset == None:
+        offset = 0
+    if songlist == None:
+        songlist = []
+    REQUEST_SONGS = "%srecording?artist=%s&limit=100&offset=%s&fmt=json" % (BASE_URL, artistId, str(offset))
+    print(REQUEST_SONGS)
+    requestSongs = urllib.request.urlopen(urllib.request.Request(REQUEST_SONGS)).read().decode("utf-8")
+    songs = json.loads(requestSongs)
+    recordingCount = songs.get("recording-count")
+    songlist.extend(songs.get("recordings"))
+    while len(songlist) < recordingCount:
+        offset += 100
+        getSongsByArtistId(artistId, offset, songlist)
+    else:
+        print(len(songlist))
+        with open("data/%s_raw.json" % (artistId), 'w') as songsFile:
+            json.dump(songlist, songsFile, indent=4)
+
+        songlist = sorted(songlist, key = lambda song: song.get("title"))
+    return songlist
+
+##TODO determine if it is faster to remove unneccessary fields first then sort and shrink, or vice versa
+
+def cleanList(songlist, artistId):
+    
+    print("Cleaning up list of %s's recordings" % (artistId))
+
+    songlistClean = []
+    first = songlist[0]
+    first.pop("video")
+    first.pop("length")
+    first.pop("disambiguation")
+    first.update({"title" : unidecode(first.get("title")), "albums" : None, "votes" : 0})
+    songlistClean.append(first)
+    print(songlistClean)
+
+    ##TODO Optimise
+    for song in songlist:
+        #songTitle = song.get("title").lower().replace(" ", "").replace(",", "").replace("’", "").replace("'", "").replace("&", "and")
+        songTitle = song.get("title").lower()
+        if '(' in songTitle:
+            start = songTitle.rfind('(')
+            end = songTitle.rfind(')')
+            ##print("start: " + str(start))
+            ##print("end: " + str(end))
+            songTitle = songTitle.replace(songTitle[start-1:end+1], "").replace(" ", "").replace(",", "").replace("’", "").replace("'", "").replace("&", "and")
+            songTitle = unidecode(songTitle)
+            ##print(songTitle)
+
+        else:
+            songTitle = song.get("title").lower().replace(" ", "").replace(",", "").replace("’", "").replace("'", "").replace("&", "and")
+            songTitle = unidecode(songTitle)
+
+        print(songTitle)
+        
+        for songC in songlistClean:
+            songCTitle = songC.get("title").lower().replace(" ", "").replace(",", "").replace("’", "").replace("'", "").replace("&", "and")
+            songCTitle = unidecode(songCTitle)
+    
+            if songCTitle in songTitle or songTitle in songCTitle:
+                print("It exists")
+                break
+
+        else:
+            print(song)
+            song.pop("video")
+            song.pop("length")
+            song.pop("disambiguation")
+            ##TODO Find a less hacky workaround
+
+            #songTitle = song.get("title").replace("\u2013", "-").replace("\u2014", "-").replace("\u2019", "'")
+            songTitle = song.get('title')
+            if '(' in songTitle:
+                start = songTitle.rfind('(')
+                end = songTitle.rfind(')')
+                ##print("start: " + str(start))
+                ##print("end: " + str(end))
+                songTitle = songTitle.replace(songTitle[start-1:end+1], "")
+                ##print(songTitle)
+
+            #song.update({"title" : songTitle.replace("\u2013", "-").replace("\u2014", "-").replace("\u2019", "'"), "albums" : None})
+            song.update({"title" : songTitle, "albums" : None, "votes" : 0})
+            songlistClean.append(song)
+            print(json.dumps(song, indent = 4))
+
+    ##print(json.dumps(songlistClean, indent=4))
+
+    ##print(len(songlist))
+    ##print(len(songlistClean))
+
+    with open("data/%s_clean.json" % (artistId), 'w') as songsFile:
+        json.dump(songlistClean, songsFile, indent=4)
+
+    return songlistClean
+
+##getAlbumsOfSong("young+and+beautiful", "b7539c32-53e7-4908-bda3-81449c367da6")
+def getAlbumsOfSong(songTitle, artistId):
+    print("Getting albums each recording is published in")
+    QUERY_SONG = songTitle.lower().replace(" ", "+").replace("\u2013", "-").replace("\u2014", "-").replace("\u2019", "'")
+    REQUEST_ALBUMS = BASE_URL + "recording/?query=" + QUERY_SONG + "+AND+arid%3A\"" + artistId + "\"&release|status=official|type=album&limit=50&fmt=json"
+    print(REQUEST_ALBUMS)
+    requestAlbums = urllib.request.urlopen(urllib.request.Request(REQUEST_ALBUMS)).read().decode("utf-8")
+    albums = json.loads(requestAlbums).get("recordings")
+    
+    albumsClean = []
+    for entry in albums:
+        entryClean = {}
+        if QUERY_SONG.replace("+", "") in entry.get("title").lower().replace(" ", ""):
+
+            for entryC in albumsClean:
+                try:
+                    if entry.get("releases")[0].get("id") == entryC.get("id") or entry.get("releases")[0].get("title") in entryC.get("releases") or entryC.get("releases") in entry.get("releases")[0].get("title"):
+                        break
+                except:
+                    pass
+            else:
+                try:
+                    entryClean.update({"id": entry.get("releases")[0].get("id"), "releases" : entry.get("releases")[0].get("title").replace("\u2013", "-").replace("\u2014", "-").replace("\u2019", "'")})
+                    albumsClean.append(entryClean)
+                except:
+                    pass
+                
+    print(json.dumps(albumsClean, indent = 4))
+    return albumsClean
+
+def getSongsByArtist(artist, getAlbumsOfSongs = False):
+    artistId = getArtistId(artist)
+
+    songlist = cleanList(getSongsByArtistId(artistId), artistId)
+    ##TODO Optimise
+    if getAlbumsOfSongs:
+        for song in songlist:
+            albumlist = getAlbumsOfSong(song.get("title"), artistId)
+            print(albumlist)
+            song.update({"albums" : albumlist})
+            print(json.dumps(song, indent = 4))
+        with open("data/%s_clean.json" % (artistId), 'w') as songsFile:
+            json.dump(songlist, songsFile, indent=4)
+        ##console.log(json.dumps(songlist, indent=4))
+    return songlist
+
+##TODO Add versions. If version does not match, delete file and rebuild
+##TODO Add force update
+def buildArtist(artistName, getAlbumsOfSongs = False):
+    artistId = getArtistId(artistName)
+
+    if os.path.isfile('./data/%s.json' % (artistId)):
+        print("Artist already exists")
+        with open('./data/%s.json' % (artistId), 'r') as artistFile:
+            return json.load(artistFile)
+
+    print("Building %s's artist profile" % (artistName))
+    artist = {"artistId" : artistId, "artistName" : artistName}
+
+    if os.path.isfile('./data/%s_clean.json' % (artistId)):
+        print('./data/%s_clean.json' % (artistId))
+        with open('./data/%s_clean.json' % (artistId), 'r') as songsFile:
+            songs = json.load(songsFile)
+
+    else:
+        songs = getSongsByArtist(artistName, getAlbumsOfSongs)
+
+    ##Build Artist
+    artist.update({"recordings-count" : len(songs), "recordings-order" : Sort.ALPHABETICAL.value, "recordings" : songs, "relationships" : {}})
+    print(json.dumps(artist, indent = 4))
+
+    with open('data/%s.json' % (artistId), 'w') as artistFile:
+        json.dump(artist, artistFile, indent = 4)
+
+    return artist
+
+def buildArtistWithId(artistId, getAlbumsOfSongs = False):
+    if os.path.isfile('./data/%s.json' % (artistId)):
+        print("Artist already exists")
+        with open('./data/%s.json' % (artistId), 'r') as artistFile:
+            return json.load(artistFile)
+
+    print("Building %s's artist profile" % (artistName))
+    artist = {"artistId" : artistId, "artistName" : artistName}
+
+    if os.path.isfile('./data/%s_clean.json' % (artistId)):
+        print('./data/%s_clean.json' % (artistId))
+        with open('./data/%s_clean.json' % (artistId), 'r') as songsFile:
+            songs = json.load(songsFile)
+
+    else:
+        songs = getSongsByArtist(artistName, getAlbumsOfSongs)
+
+    ##Build Artist
+    artist.update({"recording-count" : len(songs), "recordings-order" : Sort.ALPHABETICAL.value, "recordings" : songs, "relationships" : {}})
+    print(json.dumps(artist, indent = 4))
+
+    with open('data/%s.json' % (artistId), 'w') as artistFile:
+        json.dump(artist, artistFile, indent = 4)
+
+    return artist
+
+def addRelationship(secondArtistName, mainArtistName):
+
+    mainId = getArtistId(mainArtistName)
+    secondId = getArtistId(secondArtistName)
+
+    ##Does it automatically see which lines have changed and update those only? Or do I have to do it manually?
+    if os.path.isfile('./data/%s.json' % (mainId)):
+        with open('./data/%s.json' % (mainId), 'r') as mainArtistFile:
+            mainArtist = json.load(mainArtistFile)
+    else:
+        mainArtist = buildArtist(mainArtistName)
+
+    if os.path.isfile('./data/%s.json' % (secondId)):
+        with open('./data/%s.json' % (secondId), 'r') as secondArtistFile:
+            secondArtist = json.load(secondArtistFile)
+    else:
+        secondArtist = buildArtist(secondArtistName)
+
+    print(json.dumps(mainArtist, indent = 4))
+    print(json.dumps(secondArtist, indent = 4))
+
+    ##TODO Optimise
+    if secondId in mainArtist.get("relationships"):
+        print("Relationship exists, incrementing...")
+        mainArtist["relationships"][secondId] += 1
+
+    else:
+        print("Relationship does not exist, adding...")
+        mainArtist["relationships"][secondId] = 1
+
+    if mainId in secondArtist.get("relationships"):
+        print("Relationship exists, incrementing...")
+        secondArtist["relationships"][mainId] += 1
+
+    else:
+        print("Relationship does not exist, adding...")
+        secondArtist["relationships"][mainId] = 1
+
+    print("New relationship value is %s" % str(mainArtist.get("relationships").get(secondId)))
+
+    with open('./data/%s.json' % (mainId), 'w') as mainArtistFile:
+        json.dump(mainArtist, mainArtistFile, indent = 4)
+
+    with open('./data/%s.json' % (secondId), 'w') as secondArtistFile:
+        json.dump(secondArtist, secondArtistFile, indent = 4)
+
+    return(mainArtist.get("relationships").get(secondId))
+
+def upvote(artistName, trackName):
+    print("UPVOTE CALLED")
+    artistId = getArtistId(artistName)
+    print(artistId)
+    print(artistName)
+    print(trackName)
+
+    artist = buildArtistWithId(artistId)
+    for track in artist.get('recordings'):
+        if track.get('title') == trackName:
+            print("HIT")
+            track['votes'] += 1 
+
+            with open('./data/%s.json' % (artistId), 'w') as file:
+                json.dump(artist, file, indent = 4)
+
+            return artist
+
+    return -1
+
+class Sort(Enum):
+    ALPHABETICAL = 1
+    VOTES = 2
+
+def sortRecordings(artistRep, artistInput, sortOrder = Sort.ALPHABETICAL):
+    print("Sorting recordings of %s by %s" % (artistRep, sortOrder))
+
+    if artistInput == ArtistInput.ARTIST_NAME:
+        artistRep = getArtistId(artistRep)
+
+    with open('./data/%s.json' % (artistRep), 'r') as file:
+        artist = json.load(file)
+    recordings = artist.get('recordings')
+    recordingsOrder = Sort(artist.get('recordings-order'))
+
+    if sortOrder != recordingsOrder:
+        if sortOrder == Sort.ALPHABETICAL:
+            recordingsSorted = sorted(recordings, key = itemgetter('title'))
+        else:
+            recordingsSorted = sorted(recordings, key = itemgetter('votes'),reverse = True)
+    artist.update({'recordings-order' : sortOrder.value, 'recordings' : recordingsSorted})
+
+    return artist
+
+
+SPOTIFY_BASE = 'https://spotifycharts.com/'
+SPOTIFY_DEFAULT = '%s/regional/global/daily/latest/download' % SPOTIFY_BASE
+
+def getCharts():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+    requestCharts = urllib.request.urlretrieve(SPOTIFY_DEFAULT, 'charts.csv')
+
+    #TODO Most likely can be removed
+    with open('charts.csv', 'r', encoding="utf8") as file, open('charts_clean.csv', 'w') as output:
+        next(file)
+        for line in file:
+            output.write(line)
+
+    charts = []
+    with open('charts_clean.csv', 'r') as file:
+        chartsCSV = csv.DictReader(file)
+        line = 1
+        for row in chartsCSV:
+            charts.append({'position' : row['Position'], 'trackName' : row['Track Name'], 'artist' : row['Artist'], 'streams' : row['Streams'], 'url' : row['URL']})
+    #print(charts)
+
+    ##Reset header, if not musicbrainz calls do not work
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0')]
+    urllib.request.install_opener(opener)
+
+    return charts
